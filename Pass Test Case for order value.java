933d774f141f1f2e8150cf7e8adf02ff15b081db
saravanasara09 <142519377+saravanasara09@users.noreply.github.com>
2023-08-28 16:19:20 +0530

    Part3:Solution

---
+++ Pass Test Case for order value.java
@@ -0,0 +1,77 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantTest {
+    Restaurant restaurant;
+
+    @BeforeEach
+    public void setUp() {
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant = new Restaurant("Amelie's cafe", "Chennai", openingTime, closingTime);
+        restaurant.addToMenu("Sweet corn soup", 119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+    }
+
+
+    @Test
+    public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
+        LocalTime currentTime = LocalTime.parse("15:00:00");
+        restaurant.setCurrentTime(currentTime);
+        assertTrue(restaurant.isRestaurantOpen());
+    }
+
+    @Test
+    public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
+        LocalTime currentTime = LocalTime.parse("09:00:00");
+        restaurant.setCurrentTime(currentTime);
+        assertFalse(restaurant.isRestaurantOpen());
+
+    }
+
+// Fail test case before the order velue feature develop
+    @Test
+    public void get_order_value_should_throw_exception_for_item_not_found() {
+        List<String> selectedItems = new ArrayList<>();
+        selectedItems.add("Sweet corn soup");
+        selectedItems.add("Fried Rice"); // Assuming this item is not in the menu
+
+        assertThrows(itemNotFoundException.class,
+                () -> restaurant.getOrderValue(selectedItems));
+    }
+
+    // Test case After the order value feature developed
+    @Test
+    public void get_order_value_should_return_correct_order_value_for_selected_items() throws itemNotFoundException {
+        List<String> selectedItems = new ArrayList<>();
+        selectedItems.add("Sweet corn soup");
+        selectedItems.add("Vegetable lasagne");
+
+        int orderValue = restaurant.getOrderValue(selectedItems);
+        int expectedOrderValue = 119 + 269;
+        assertEquals(expectedOrderValue, orderValue);
+    }
+
+    @Test
+    public void adding_item_to_menu_should_increase_menu_size_by_1(){
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.addToMenu("Sizzling brownie",319);
+        assertEquals(initialMenuSize+1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.removeFromMenu("Vegetable lasagne");
+        assertEquals(initialMenuSize-1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_that_does_not_exist_should_throw_exception() {
+        assertThrows(itemNotFoundException.class,
+                ()->restaurant.removeFromMenu("French fries"));
+    }
+
+}
